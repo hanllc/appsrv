ebb5a9d4275a52d8a54031645100e8970952273f
joe <joe@wehrli.org>
2015-06-21 15:10:44 -0400

    updated fcgi hello world from clang tested and success

---
+++ cpplang/fcgi-run.sh
@@ -0,0 +1,1 @@
+spawn-fcgi -p 8000 -n myapp.fcgi
---
+++ cpplang/myapp-fcgi.cpp
@@ -0,0 +1,12 @@
+#include <fcgi_stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int count = 0;
+    while(FCGI_Accept() >= 0)
+        printf("Content-type: text/html\r\n\r\n"
+               "Request number %d on host BLOW <i>%s</i>\n",
+               ++count, getenv("SERVER_NAME"));
+    return 0;
+}
